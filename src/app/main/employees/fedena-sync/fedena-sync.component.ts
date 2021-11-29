c89e704a173adb906177aeb3af779441b9713461
ShahendaKhaled <shahenda.khaled@trianglz.com>
2021-11-29 15:05:17 +0200

    Fix merge issues

--- src/app/main/employees/fedena-sync/fedena-sync.component.ts
+++ src/app/main/employees/fedena-sync/fedena-sync.component.ts
@@ -51,7 +51,7 @@ getSyncStatus(initialCheck: boolean = false) {
        }); 
 }
   syncFedena() {
-    this.fedenaSyncService.sync().subscribe(
+    this.fedenaSyncService.hrSync().subscribe(
       (response: any) => {
           this.intervalTime = 1000*60*10; // 10 minutes Initial time
           this.isSyncing = true;
