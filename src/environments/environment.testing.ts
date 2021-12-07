860a868f799947fe5e418f016e23f44b9a82dd64
Ashour15 <mohamedashour15@hotmail.com>
2021-12-07 18:16:53 +0200

    Add necessary files for buidling an angular environment

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    env: 'development',
+    firebase: {
+      apiKey: 'AIzaSyBrhbe7aBAfASoiiVoR4tIuThO1IGmnKDU',
+      authDomain: 'bedopedia-225ee.firebaseapp.com',
+      databaseURL: 'https://bedopedia-225ee.firebaseio.com',
+      projectId: 'bedopedia-225ee',
+      storageBucket: 'bedopedia-225ee.appspot.com',
+      messagingSenderId: '742721869641',
+      appId: '1:742721869641:web:2e4ac2d223b4663e7d2c98'
+    }
+};
+
+export const baseUrl = ' https://testing1.skoleralms.com/';
+export const siteKey = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
+
