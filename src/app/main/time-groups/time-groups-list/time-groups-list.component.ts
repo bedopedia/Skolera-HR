a922eb20f1c9048fa50ba8743637355101867441
Yasmin Abdelhay <yasminabdelhay7@gmail.com>
2022-02-20 17:35:29 +0200

    [SKLT-2214]  [Fingerprint][translation] the type in time group list should be translated correctly  (#67)
    * fix types translation bug
    
    * reload after the changing of translation
    
    * return tr_types to be tr_type
    
    * fix clear test case

--- src/app/main/time-groups/time-groups-list/time-groups-list.component.ts
+++ src/app/main/time-groups/time-groups-list/time-groups-list.component.ts
@@ -19,9 +19,12 @@ export class TimeGroupsListComponent implements OnInit {
   timeGroupsLoading: boolean = true;
   timeGroupsList: TimeGroup[] = [];
   paginationPerPage = 10;
-  selectedtype: string;
+  selectedtype:{
+    value: string,
+    text: string
+  };
   paginationData: PaginationData;
-  timeGroupsType = ['fixed', 'shifts']
+  timeGroupsType = [{text:this.translateService.instant('tr_fixed'), value: 'fixed'}, {text:this.translateService.instant('tr_shifts'), value: "shifts"}]
   isFilterOpen: boolean = false;
   checkedCells: number[] = [];
   allChecked: boolean = false;
@@ -123,8 +126,11 @@ export class TimeGroupsListComponent implements OnInit {
   filterTimeGroups(event: any, searchKey: string) {
     clearTimeout(this.searchTimeout);
         this.searchTimeout = setTimeout(() => {
-            let term = (searchKey == 'by_type') ? event : event.target.value.trim()
-            if (event === null) {
+            let term ;
+            if(event){
+              term = (searchKey == 'by_type') ? event.value : event.target.value.trim()
+            }
+            if (event == null) {
               delete this.params['by_type'];
             }
             else {
